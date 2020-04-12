7d6ad0c929d33adaa24915a9816035019bd7d0d9
garima <garimajain096@gmail.com>
2020-04-13 01:15:50 +0530

    firstcommit

---
+++ routes/authentication.js
@@ -0,0 +1,20 @@
+const express=require('express');
+var router=express.Router();
+const {check}=require('express-validator');
+//importing authorization controller
+const {signout,signup,signin,isSignedIn }=require("../controllers/authentication")
+
+router.post("/signup",[
+    check("user_name").isLength({min:5}).withMessage('must be at least 5 charcters long'),
+    check("email").isEmail().withMessage('email is required'),
+    check("password").isLength({min:5}).withMessage('must be at least 5 charcters long'),
+    
+
+],signup);
+
+router.post("/signin/:email/:password",signin);
+
+
+
+
+module.exports=router;  
